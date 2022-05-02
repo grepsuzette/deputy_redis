9a67b1589a045a7f386cdc268e779232ae85c5ec
grepsuzette <grepsuzette@users.noreply.github.com>
2022-05-02 13:43:32 +0800

    a first minimal try at options

--- src/deputy/redis/Redis.hx
+++ src/deputy/redis/Redis.hx
@@ -10,6 +10,15 @@ class Redis {
     /**
      * @note The defaults set when opts is null have proven to work in our case.
      * See https://github.com/luin/ioredis/issues/1123
+     * @param (opts) There is no formalization yet for the options. 
+     * But for js, try these:
+     *   https://luin.github.io/ioredis/interfaces/CommonRedisOptions.html
+     *   https://luin.github.io/ioredis/interfaces/SentinelConnectionOptions.html
+     *   - connectTimeout: Int
+     *   - disconnectTimeout: Int
+     *   - commandTimeout: Int         If a command does not return a reply 
+     *                                  within a set number of milliseconds,
+     *                                  a "Command timed out" error will be thrown.
      */
     public static inline function usable(opts:{}=null) : tink.core.Promise<RedisClient> {
         #if js
@@ -18,7 +27,15 @@ class Redis {
             enableOfflineQueue   : false,
             lazyConnect          : true
         }
-        return deputy.redis.impl.js.ImplJs.usable(opts == null ? DEFAULTOPTS : opts)
+        // in js, enableAutoPipelining, enableOfflineQueue, lazyConnect are
+        // always needed, thus if not overriden we use the defaults
+        var o = {}
+        for (field in Reflect.fields(DEFAULTOPTS)) Reflect.setField(o, field, Reflect.field(DEFAULTOPTS, field));
+        if (opts != null) {
+            for (field in Reflect.fields(opts))
+                Reflect.setField(o, field, Reflect.field(opts, field));
+        }
+        return deputy.redis.impl.js.ImplJs.usable(o)
             .next(x -> new RedisClient(x));
         #else
         #error "unsupported target (for now)"
